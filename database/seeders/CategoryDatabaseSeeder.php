f360a83c1fd4f22454b27dddc6a7fab47645b990
mario george <mariogcool@gmail.com>
2023-02-19 16:23:05 +0200

    =end branch settings

---
+++ database/seeders/CategoryDatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+
+class CategoryDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Category::factory()->count(20)->create();
+      //  factory(App\ModelsCategory::class, 20)->create();
+    }
+}
---
+++ lang/en/admin/profile.php
@@ -0,0 +1,8 @@
+<?php
+
+return[
+    'name required'=>'name is required',
+    'email required'=>'email is required',
+    'email email'=>'email must be real email',
+    'email unique'=>'email must be unique'
+];
